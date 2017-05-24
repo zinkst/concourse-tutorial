a580de96f8d1aa4fcdd659ad8e791fbec9cbe0a0
Stefan Zink <Stefan.Zink@de.ibm.com>
2017-05-24 14:15:03 +0200

    modify manifest.yml

--- 15_deploy_cloudfoundry_app/update-manifest.sh
+++ 15_deploy_cloudfoundry_app/update-manifest.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 pwd
 ls -al
-#cp resource-web-app/manifest.yml cf-manifest/
-echo "hello my task"
+cp resource-web-app/manifest.yml cf-manifest/
+echo "  buildpack: go_buildpack" >> cf-manifest/manifest.yml
+echo "  random-route: true" >> cf-manifest/manifest.yml
+cat cf-manifest/manifest.yml
